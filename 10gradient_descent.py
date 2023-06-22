343dcf0c15324350853bbb1f536eeb7626567eca
Siarhei Tratsiak <serafim619@gmail.com>
2023-06-22 12:12:52 +0200

    gradient descent

---
+++ 10gradient_descent.py
@@ -0,0 +1,11 @@
+weight = 0.5
+goal_pred = 0.8
+input = 0.5
+
+for iteration in range(20):
+    pred = input * weight
+    error = (pred - goal_pred) ** 2
+    direction_and_amount = (pred - goal_pred) * input
+    weight -= direction_and_amount
+
+    print("Error: {} Prediction: {}".format(error, pred))
